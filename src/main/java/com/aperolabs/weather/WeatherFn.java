b08cd964b5374b59b897beabdb18841c8ca5207e
Travis <longoria.public@gmail.com>
2019-09-23 11:28:15 -0600

    add weather units as configurable property

--- src/main/java/com/aperolabs/weather/WeatherFn.java
+++ src/main/java/com/aperolabs/weather/WeatherFn.java
@@ -1,6 +1,7 @@
 package com.aperolabs.weather;
 
 import com.aperolabs.weather.models.Weather;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
@@ -11,14 +12,17 @@ import java.util.function.Function;
 @SpringBootApplication
 public class WeatherFn {
 
+    @Value("${weather.units}")
+    private String units;
+
     @Component
     public class Forecast implements Function<String, String> {
-        final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={appid}&units=imperial";
+        final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={appid}&units={units}";
 
         public String apply(String zip) {
             final String API_KEY = System.getenv("OPEN_WEATHER_API_KEY");
             RestTemplate restTemplate = new RestTemplate();
-            Weather weather = restTemplate.getForObject(WEATHER_URL, Weather.class, zip, API_KEY);
+            Weather weather = restTemplate.getForObject(WEATHER_URL, Weather.class, zip, API_KEY, units);
 
             return weather.getMain().getTemp().toString();
         }
